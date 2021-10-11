bb8931367d46a474bcffda9b1545fedaae7ef687
enriquemosso65 <enriquemosso65@aragon.unam.mx>
2021-10-11 17:26:05 -0500

    barra de progreso, correcion de color

--- app.js
+++ app.js
@@ -57,3 +57,18 @@ toggle.onclick = function(){
     toggle.classList.toggle('active');
     sidebar.classList.toggle('active');
 }
+document.addEventListener(
+  "scroll",
+  function() {
+    var scrollTop =
+      document.documentElement["scrollTop"] || document.body["scrollTop"];
+    var scrollBottom =
+      (document.documentElement["scrollHeight"] ||
+        document.body["scrollHeight"]) - document.documentElement.clientHeight;
+    scrollPercent = scrollTop / scrollBottom * 100 + "%";
+    document
+      .getElementById("_progress")
+      .style.setProperty("--scroll", scrollPercent);
+  },
+  { passive: true }
+);
